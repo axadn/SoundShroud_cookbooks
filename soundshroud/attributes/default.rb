2203be255d21ccb795433bc28d43fa34a8a8dce5
Adan Marrufo <adanmarrufo@gmail.com>
2018-04-17 23:36:24 -0700

    add environment variables to soundshroud cookbook attributes

--- soundshroud/attributes/default.rb
+++ soundshroud/attributes/default.rb
@@ -14,7 +14,11 @@ default[:soundshroud][:environment] = {
   "SECRET_KEY_BASE": node[:soundshroud][:secret_key_base],
   "DATABASE_URL": node[:soundshroud][:database_url],
   "RAILS_ENV": node[:soundshroud][:rails_env],
-  "RAILS_LOG_TO_STDOUT": node[:soundshroud][:log_to_stdout]
+  "RAILS_LOG_TO_STDOUT": node[:soundshroud][:log_to_stdout],
+  "S3_BUCKET": node[:soundshroud][:s3_bucket],
+  "S3_KEY": node[:soundshroud][:s3_key],
+  "S3_ID": node[:soundshroud][:s3_id],
+  "S3_URL": node[:soundshroud][:s3_url]
 }
 
 default[:soundshroud][:start_cmd] = "unicorn -E production -c /opt/unicorn.rb"
