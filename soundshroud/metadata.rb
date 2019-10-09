bbbff96f338fecd98ad51336a778baf6684dc343
Adan Marrufo <adanmarrufo@gmail.com>
2019-10-10 01:43:35 +0800

    add redis dependency

--- soundshroud/metadata.rb
+++ soundshroud/metadata.rb
@@ -5,6 +5,7 @@ license     "Apache 2.0"
 version     "1.0.0"
 
 depends 'apt', '= 6.1.4'
+depends 'redis'
 depends 'ruby-ng'
 depends 'nodejs'
 depends 'postgres'
--- soundshroud/recipes/setup.rb
+++ soundshroud/recipes/setup.rb
@@ -3,6 +3,7 @@
 include_recipe "ruby-ng::dev"
 include_recipe "nodejs"
 include_recipe "nodejs::npm"
+include_recipe "redis"
 include_recipe "postgres"
 include_recipe "nginx"
 include_recipe "unicorn"
