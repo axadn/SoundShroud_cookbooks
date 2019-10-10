7ccf57d80c944e56b1622b17e94b05e793babbe5
Adan Marrufo <adanmarrufo@gmail.com>
2019-10-10 16:56:57 +0800

    remove duplicate redis command from setup recipe

--- soundshroud/recipes/setup.rb
+++ soundshroud/recipes/setup.rb
@@ -3,8 +3,6 @@
 include_recipe "ruby-ng::dev"
 include_recipe "nodejs"
 include_recipe "nodejs::npm"
-include_recipe "runit"
-include_recipe "redis"
 include_recipe "redis::server"
 include_recipe "postgres"
 include_recipe "nginx"
