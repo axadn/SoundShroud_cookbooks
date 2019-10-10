8f3f605a580c6b827a1c227dc5190a8dfd020d3b
Adan Marrufo <adanmarrufo@gmail.com>
2019-10-11 01:11:41 +0800

    add libsox apt package to setup script

--- soundshroud/recipes/setup.rb
+++ soundshroud/recipes/setup.rb
@@ -10,6 +10,9 @@ include_recipe "unicorn"
 
 apt_package 'zlib1g-dev'
 apt_package 'libpq-dev'
+apt_package 'libsox-fmt-all'
+apt_package 'sox'
+apt_package 'libchromaprint-dev'
 
 template "soundshroud_service" do
     path "/etc/init.d/soundshroud"
